7fefcc14add9d17443f47ec4d5a55b8bcdc05a72
Alex <saswork@hotmail.com>
2016-05-24 12:50:43 +0300

    added @angular/http

---
+++ src/app/shared/data.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import { Http } from '@angular/http';
+import { Observable } from 'rxjs/Observable';
+
+@Injectable()
+export class DataService {
+
+  constructor() {}
+
+  getAds(){
+
+  }
+
+  getAd(id?: number){
+
+  }
+}
