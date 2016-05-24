55b52c5047768927e40b855ffd55dff3f128ad85
Alex <saswork@hotmail.com>
2016-05-24 13:49:10 +0300

    using http

--- src/app/ads-app.component.ts
+++ src/app/ads-app.component.ts
@@ -4,13 +4,16 @@ import { ListComponent } from './core/list/list.component';
 import { HeaderComponent } from './core/header/header.component';
 import { NewAdComponent } from "./core/new-ad/new-ad.component";
 import { AdDetailComponent } from "./core/ad-detail/ad-detail.component";
+import { HTTP_PROVIDERS } from '@angular/http';
+import 'rxjs/Rx';
 
 @Component({
     moduleId: module.id,
     selector: 'ads-app',
     templateUrl: 'ads-app.component.html',
     styleUrls: ['ads-app.component.css'],
-    directives: [ListComponent, HeaderComponent, ROUTER_DIRECTIVES]
+    directives: [ListComponent, HeaderComponent, ROUTER_DIRECTIVES],
+    providers: [HTTP_PROVIDERS]
 })
 @Routes([
     {path: '/ads',  component: ListComponent},
--- src/app/shared/data.service.ts
+++ src/app/shared/data.service.ts
@@ -1,17 +1,29 @@
 import { Injectable } from '@angular/core';
-import { Http } from '@angular/http';
+import { Http, Response } from '@angular/http';
 import { Observable } from 'rxjs/Observable';
+import { IAd } from './../core/add/ad.component';
+
 
 @Injectable()
 export class DataService {
+  private _productUrl = 'app/ads.json';
 
-  constructor() {}
+  constructor(private _http: Http) { }
 
-  getAds(){
-
+  getAds(): Observable<IAd[]> {
+    return this._http.get(this._productUrl)
+      .map((response: Response) => <IAd[]> response.json())
+      .do(data => console.log('All: ' +  JSON.stringify(data)))
+      .catch(this.handleError);
   }
 
-  getAd(id?: number){
+  getAd(id: number): Observable<IAd> {
+    return this.getAds()
+      .map((products: IAd[]) => products.find(p => p.id === id));
+  }
 
+  private handleError(error: Response) {
+    console.error(error);
+    return Observable.throw(error.json().error || 'Server error');
   }
 }
