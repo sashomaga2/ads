ba7a59128f9afc05a17568115b8c47a31d8cb6d4
Alex <saswork@hotmail.com>
2016-05-24 23:14:36 +0300

    refactor directories

--- src/app/shared/services/data.service.ts
+++ src/app/shared/services/data.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Http, Response } from '@angular/http';
-import { Observable } from '../../../../node_modules/rxjs/Observable';
+import { Observable } from 'rxjs/Observable';
 import { IAd } from './../../core/add/ad.component';
 
 
