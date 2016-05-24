133d20a62fd7a8c8bfb6e91af528613562e7af0c
Alex <saswork@hotmail.com>
2016-05-24 22:45:32 +0300

    refactor directories

--- src/app/core/list/list.component.ts
+++ src/app/core/list/list.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { IAd, AdComponent } from './../add/ad.component';
-import { DataService } from '../../shared/data.service';
+import { DataService } from '../../shared/services/data.service';
 
 @Component({
     moduleId: module.id,
--- src/app/shared/services/data.service.ts
+++ src/app/shared/services/data.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Http, Response } from '@angular/http';
-import { Observable } from 'rxjs/Observable';
-import { IAd } from './../core/add/ad.component';
+import { Observable } from '../../../../node_modules/rxjs/Observable';
+import { IAd } from './../../core/add/ad.component';
 
 
 @Injectable()
