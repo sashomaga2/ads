da4a8b5e80a103d3d01c1b192e31e15573f93a3d
Aleksandar Marinov <Aleksandar.Marinov@paddypower.com>
2016-05-26 11:58:16 +0100

    start add node.js server

---
+++ src/server/app.js
@@ -0,0 +1,9 @@
+/**
+ * Temporaty put in src until https://github.com/angular/angular-cli/issues/677 is resolved
+ */
+
+const express = require('express');
+
+const app = express();
+
+console.log("app", app);
