65946f00ca64bb1c1ccb1f6385521bdb5be14a16
Aleksandar Marinov <Aleksandar.Marinov@paddypower.com>
2016-05-30 13:19:24 +0100

    add gulp typescript compile

--- gulpfile.js
+++ gulpfile.js
@@ -1,1 +1,14 @@
 var gulp = require('gulp');
+var ts = require('gulp-typescript');
+
+gulp.task('compile-ts', function () {
+    return gulp.src(['src/app/**/*.ts'])
+        .pipe(ts({
+            noImplicitAny: true,
+            experimentalDecorators: true,
+            isolatedModules: true,
+            emitDecoratorMetadata: true,
+            module: "commonjs"
+        }))
+        .pipe(gulp.dest('dist/app'));
+});
